b4ae52061c8021abb39ab2e14567077badd3c07f
Ivorra-1996 <ivorrajosemaria@gmail.com>
2022-04-25 00:50:31 -0300

    Terminado

---
+++ insercion_eliminacion.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+
+/*Pasamos por parametro el nombre de la base de datos, el usuario y la pass*/
+const sequelize = new Sequelize('usuarios','root','1234',{
+    host:'localhost', /*Ubicacion de la base de datos*/
+    dialect :'mariadb' /*Tipo de base de datos*/
+});
+
+
+/*Realiza una autenticacion de conexion con la base de datos*/
+sequelize.authenticate().then(() => {
+    console.log('Conexion estableciada :)');
+}).catch(err => {
+    console.error('No se pudo conectar a la base de datos:',err);
+});
+
+/*Creamos la clase hija heredada de la Clase Padre, con su constructor*/
+class Usuarios extends Sequelize.Model {}
+Usuarios.init({
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    age: Sequelize.STRING,
+    dni: Sequelize.STRING
+},{ sequelize, modelName :'Users' });
+
+
+/* Crea un usuario y lo inserta en la tabla 'Users' como registro.*/
+sequelize.sync()
+  .then(() => Usuarios.create({
+    firstName: 'Marcelo',
+    lastName: 'Ivorra',
+    age: '26',
+    dni: '12817672'
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
+
+  //elimina usuario con id =2
+Usuarios.destroy({
+    where: {
+      firstName: 'Gaston'
+    }
+  }).then(() => {
+    console.log("Elimine Registro");
+  });
+  
